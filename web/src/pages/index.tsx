c3aaca23f7c9a67419474336f72d5fc12057de3b
洛竹 <youngjuning@aliyun.com>
2021-08-16 15:39:48 +0800

    chore: add demo

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,9 +1,28 @@
-import styles from './index.less';
+import { useEffect, useState } from 'react';
 
-export default function IndexPage() {
+require('./index.less');
+
+const cats = {
+  'Coding Cat': 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
+  'Compiling Cat': 'https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif',
+};
+
+const IndexPage = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount(v => {
+        return v + 1;
+      });
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  const cat = count % 2 ? 'Compiling Cat' : 'Coding Cat';
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <img src={cats[cat]} width="300" alt="cat" />
     </div>
   );
-}
+};
+
+export default IndexPage;
