a61ba1793c87b54760a21587158f49d327a29352
洛竹 <youngjuning@aliyun.com>
2021-08-18 10:45:33 +0800

    chore: bump @luozhu/vscode-utils version to 0.2.3

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -6,6 +6,11 @@ const HomePage = () => {
   return (
     <>
       <h1 className={styles.title}>Juejin Posts</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus eligendi neque
+        reprehenderit nemo corporis eveniet, soluta placeat enim iste ad ipsa consequuntur explicabo
+        totam doloribus eum mollitia in quam! Vel.
+      </p>
     </>
   );
 };
