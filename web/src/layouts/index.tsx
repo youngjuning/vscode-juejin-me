ec6c86f2ece9c230aa29025c657071313b202f69
洛竹 <youngjuning@aliyun.com>
2021-08-23 12:00:14 +0800

    fix: 'zh-cn' -> zh-CN

--- web/src/layouts/index.tsx
+++ web/src/layouts/index.tsx
@@ -4,7 +4,7 @@ import Channel from '@luozhu/vscode-channel';
 require('./index.less');
 
 window.channel = new Channel();
-setLocale(window.vscodeEnv.language, false);
+setLocale(window.vscodeEnv.language === 'zh-cn' ? 'zh-CN' : window.vscodeEnv.language, false);
 
 export default props => {
   return (
